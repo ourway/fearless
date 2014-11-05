57462b804a4f4ad811efa1dd709516fb3de82eb9
Farsheed Ashouri <farsheed@farsheed.ws>
2014-11-05 04:39:33 +0330

    repo *None* created successfully | Added fearless.rst to repository

---
+++ src/views/showtime/js/showtimeCtrls.js
@@ -0,0 +1,10 @@
+var fearlessShowtimeApp = angular.module('fearlessShowtimeApp', ['ngResource', 'ngCookies']);
+
+
+	fearlessShowtimeApp.controller('showtimeCtrl', function($scope) {
+
+
+            console.log('ok')
+
+
+    });
