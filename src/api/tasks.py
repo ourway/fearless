61a42c9fcf6d765956a56d45f10dd25144ceee68
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2015-01-07 02:29:35 +0330

    fixed a typo

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -217,7 +217,7 @@ def duration(path):
         except ValueError:
             return
 
-@Capp.tasl
+@Capp.task
 def generateAudioThumbnail(path, assetUuid, version):
     path = path.encode("utf-8")
     #wav2png(output_filename_w=None, output_filename_s=None, image_width=146, image_height=110, fft_size=2048, f_max=22050, f_min=10, wavefile=0, palette=1, channel=1, window="hanning", logspec=0)
