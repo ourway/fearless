79278c807c13812a9da879de34d75947fe7767dc
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2015-01-29 15:34:17 -0800

    going to write task writer from auto planner

--- src/api/templates/TPlans/tplans_tests.py
+++ src/api/templates/TPlans/tplans_tests.py
@@ -4,8 +4,17 @@ import json as json
 from uuid import uuid4  # for random guid generation
 import base64
 from collections import OrderedDict
+import datetime
 
-
+class Task:
+    effort = 0
+    duration = 0
+    depends = []
+    uuid = None
+    resources = []
+    start = None
+    end = None
+    milestone = None
 
 
 def getUUID():
@@ -34,6 +43,7 @@ def render_process(t):
     plan = json.loads(raw_root)
     outputs = []
     last_order = 0
+    found_tasks = False
 
     #print plan
 
@@ -46,29 +56,45 @@ def render_process(t):
         for process in processes:
             depends = []
             _id = getUUID()
-            plan['processes'][process]['id'] = _id
+            plan['processes'][process]['uuid'] = _id
 
             po = int(plan['processes'][process]['order'])
-            _this = {'name':process, 'id':_id, 'order':po}
+            _this = {'name':process, 'uuid':_id, 'order':po}
             if list_of_order_numbers.count(po)>1:
                 multies.append(_this)
                 remainings = outputs
-                if po > last_order:
+                if po > last_order and outputs:
                     plan['processes'][process]['depends_on'] = outputs
-                elif po == last_order:
+                elif po == last_order and remainings:
                     plan['processes'][process]['depends_on'] = remainings
 
             else:
                 if multies:
                     outputs = multies
                     multies = []
-                plan['processes'][process]['depends_on'] = outputs
+                if outputs:
+                    plan['processes'][process]['depends_on'] = outputs
                 outputs = [_this]
             last_order = max(po, last_order)
 
 
             process_plan = render_process(plan['processes'][process]['template'])
-            plan['processes'][process]['processes'] = process_plan
+
+            if process_plan:
+                subprocesses = process_plan.get('processes')
+                subtasks = process_plan.get('tasks')
+                if subprocesses:
+                    plan['processes'][process]['processes'] = subprocesses
+                elif plan['processes'][process].get('processes'):
+                    del(plan['processes'][process]['processes'])
+
+                if subtasks:
+                    plan['processes'][process]['tasks'] = subtasks
+                elif plan['processes'][process].get('tasks'):
+                    del(plan['processes'][process]['tasks'])
+
+            else:
+                del(plan['processes'][process])
 
 
     if plan.get('tasks'):
@@ -78,18 +104,20 @@ def render_process(t):
         multies = []
         plan['tasks'] = tasks
         for task in tasks:
+            if not task:
+                continue
             _id = getUUID()
             depends = outputs
-            plan['tasks'][task]['id'] = _id
+            plan['tasks'][task]['uuid'] = _id
 
             to = int(plan['tasks'][task]['order'])
-            _this = {'name':task, 'id':_id, 'order':to}
+            _this = {'name':task, 'uuid':_id, 'order':to}
             if list_of_order_numbers.count(to)>1:
                 multies.append(_this)
                 remainings = outputs
-                if to > last_order:
+                if to > last_order and outputs:
                     plan['tasks'][task]['depends_on'] = outputs
-                elif to == last_order:
+                elif to == last_order and remainings:
                     plan['tasks'][task]['depends_on'] = remainings
 
             else:
@@ -102,8 +130,16 @@ def render_process(t):
 
             if plan['tasks'][task].get('template'):
                 process_task = render_task(plan['tasks'][task]['template'])
-                plan['tasks'][task]['task'] = process_task
+                if process_task:
+                    found_tasks = True
+                    plan['tasks'][task]['plan'] = process_task
+            else:
+                del(plan['tasks'][task])
+                print('\tWarning: task "%s -> %s" has not any template!' % (t, task))
 
+
+    #if not found_tasks:
+    #    return {}
 
     with open('plan.json', 'wb') as f:
         f.write(json.dumps(plan, sort_keys=True, indent=4))
@@ -119,10 +155,34 @@ def render_task(t):
     return task
 
 
+def flatten(plan):
+    flat = {"tasks":[]}
+    if plan.get('tasks'):
+        for i in plan.get('tasks'):
+            task = plan['tasks'][i]
+            flat["tasks"].append(task)
+    prs = plan.get('processes')
+    target = prs
+    while True:
+        if not target:
+            break
+        for i in target:
+            process = target.get(i)
+            if process.get('tasks'):
+            #if target.get('tasks'):
+                flat['tasks'].append(process.get('tasks'))
+        target = target.get('processes')
+
+
+    with open('flat.json', 'wb') as f:
+        f.write(json.dumps(flat, sort_keys=True, indent=4))
+    return flat
+
 
 if __name__ == '__main__':
     character_preproduction_template = "character_preproduction"
-    render_process(character_preproduction_template)
+    plan = render_process(character_preproduction_template)
+    flat = flatten(plan)
 
 
 
