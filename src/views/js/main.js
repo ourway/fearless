e773cd345c7b7a7aee0c016e8879e7e09fd9afed
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2015-02-19 04:26:53 -0800

    bug_fixes

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -359,7 +359,15 @@ function updateImageSize(img, maxWidth, maxHeight){
             return data;
         }
 
-
+        $scope.getReadableFileSizeString = function(fileSizeInBytes) {
+            var i = -1;
+            var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+            do {
+                fileSizeInBytes = fileSizeInBytes / 1024;
+                i++;
+            } while (fileSizeInBytes > 1024);
+            return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+        };
 
 
         $scope.getComments = function(){
