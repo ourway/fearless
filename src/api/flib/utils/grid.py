36e094489405580d16ae179041e83e286405eeef
farsheed ashouri <farsheed.ashouri@gmail.com>
2016-01-27 17:15:28 +0330

    fixed ordering of grid

--- src/api/flib/utils/grid.py
+++ src/api/flib/utils/grid.py
@@ -1,6 +1,7 @@
 
 from flib.models import Asset, Project, Tag, User
 import ujson as json
+from sqlalchemy import desc, asc
 import itertools
 
 
@@ -9,7 +10,7 @@ class GridAssets:
 		_from = (int(page)-1) * 100
 		_to = 100
 		raws = req.session.query(Asset).\
-		    filter(Asset.tgs.any(Tag.name==tag)).offset(_from).limit(_to).all()
+		    filter(Asset.tgs.any(Tag.name==tag)).order_by(desc(Asset.created_on)).offset(_from).limit(_to).all()
 		tags = list(set(list(itertools.chain(*[i.tags for i in raws]))))
 		resp.body = raws
 
